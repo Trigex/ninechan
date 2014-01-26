016574a61e35ef690a9490d47112086b0c88318a
Julian van de Groep <julianvdg@gmail.com>
2014-01-26 14:56:12 +0100

    Created config.php
    Future configuration things will be added to this.

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+//// MySQL Database Connection ////
+$mysql=array();
+$mysql['host']="localhost"; // MySQL Host
+$mysql['user']="root"; // MySQL Username
+$mysql['pass']=""; // MySQL Password
+$mysql['data']="ninechan"; // MySQL Database
+
+//// Board Data ////
+$ninechan=array();
+$ninechan['title'] = "ninechan";
+$ninechan['desc'] = "ninechan best shitty board software";
