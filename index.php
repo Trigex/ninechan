dba3d4acadbe801602926f2050df686ad1011bda
Julian van de Groep <julianvdg@gmail.com>
2014-01-28 17:14:19 +0100

    Updated to 1.3

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ if($_GET['v']=="index") {
 	print("<h2>Threads</h2>");
 	print("<h3><a href=?v=post>New Thread</a></h3>");
 	print("<ul>");
-	$threads=mysql_query("SELECT * FROM posts WHERE op='1' ORDER BY id");
+	$threads=mysql_query("SELECT * FROM posts WHERE op='1' ORDER BY id DESC");
 	$num_rows=mysql_num_rows($threads);
 	if(!$num_rows){print("There are no threads.");}
 	while($row=mysql_fetch_array($threads)) {
@@ -42,7 +42,7 @@ if($_GET['v']=="index") {
 		print("<br /><br /><i><font size=2>".$row['date']."</font></i></fieldset>");
 	}
 	print("<h3><a href=?v=post&t=".$tid.">New Reply</a></h3>");
-} elseif(($_GET['v']=="post")) {
+} elseif($_GET['v']=="post") {
 	print("<form method=post action=?v=submit>");
 	if(isset($_GET['t'])){
 		if(!is_numeric($_GET['t'])){header('Location: ./');}
@@ -58,19 +58,19 @@ if($_GET['v']=="index") {
 		print("Title*: <input type=text name=title /><br />");
 	}
 	print("Name: <input type=text name=name /><br />Email: <input type=text name=email /><br />Comment*:<br /><textarea name=content rows=6 cols=48></textarea><br /><font size=2>* = Required</font><br /><input type=submit value=Submit /></form>");
-} elseif(($_GET['v']=="submit")) {
-if((isset($_POST['title']))&&(!$_POST['title']=="")){$title = htmlentities($_POST['title'], ENT_QUOTES | ENT_IGNORE, "UTF-8");$title = stripslashes($title);}else{die("<h2>no title entered</h2><meta http-equiv=\"refresh\" content=\"2; URL=".$_SERVER['PHP_SELF']."\">");}
-if((isset($_POST['name']))&&(!$_POST['name']=="")){$name = htmlentities($_POST['name'], ENT_QUOTES | ENT_IGNORE, "UTF-8");$name = stripslashes($name);}else{$name="Anonymous";}
-if(isset($_POST['email'])){$email = htmlentities($_POST['email'], ENT_QUOTES | ENT_IGNORE, "UTF-8");$email = stripslashes($email);}
-$date=date('d/m/Y @ g:iA T');
-if((isset($_POST['content']))&&(!$_POST['content']=="")){$content = htmlentities($_POST['content'], ENT_QUOTES | ENT_IGNORE, "UTF-8");$content = nl2br($content);$content = stripslashes($content);}else{die("<h2>no comment entered</h2><meta http-equiv=\"refresh\" content=\"2; URL=".$_SERVER['PHP_SELF']."\">");}
-$ip=base64_encode($_SERVER['REMOTE_ADDR']);
-if(!isset($_POST['tid'])){$op=1;}else{$op=0;}
-if(isset($_POST['tid'])){$tid = htmlentities($_POST['tid'], ENT_QUOTES | ENT_IGNORE, "UTF-8");$tid = stripslashes($tid);}else{$tidget=mysql_query("SELECT MAX(tid) AS tid FROM posts LIMIT 1");$num_rows=mysql_num_rows($tidget);$tid=0;while($row=mysql_fetch_array($tidget)){$tid=$row['tid'];}++$tid;}
-mysql_query("INSERT INTO `".$mysql['data']."`.`posts` (`title`,`name`,`email`,`date`,`content`,`ip`,`op`,`tid`) VALUES ('$title','$name','$email','$date','$content','$ip','$op','$tid')");
-print("<h1>posted</h1><meta http-equiv=\"refresh\" content=\"2; URL=".$_SERVER['PHP_SELF']."\">");
+} elseif($_GET['v']=="submit") {
+	if((isset($_POST['title']))&&(!$_POST['title']=="")){$title = htmlentities($_POST['title'], ENT_QUOTES | ENT_IGNORE, "UTF-8");$title = stripslashes($title);}else{die("<h2>no title entered</h2><meta http-equiv=\"refresh\" content=\"2; URL=".$_SERVER['PHP_SELF']."\">");}
+	if((isset($_POST['name']))&&(!$_POST['name']=="")){$name = htmlentities($_POST['name'], ENT_QUOTES | ENT_IGNORE, "UTF-8");$name = stripslashes($name);}else{$name="Anonymous";}
+	if(isset($_POST['email'])){$email = htmlentities($_POST['email'], ENT_QUOTES | ENT_IGNORE, "UTF-8");$email = stripslashes($email);}
+	$date=date('d/m/Y @ g:iA T');
+	if((isset($_POST['content']))&&(!$_POST['content']=="")){$content = htmlentities($_POST['content'], ENT_QUOTES | ENT_IGNORE, "UTF-8");$content = nl2br($content);$content = stripslashes($content);}else{die("<h2>no comment entered</h2><meta http-equiv=\"refresh\" content=\"2; URL=".$_SERVER['PHP_SELF']."\">");}
+	$ip=base64_encode($_SERVER['REMOTE_ADDR']);
+	if(!isset($_POST['tid'])){$op=1;}else{$op=0;}
+	if(isset($_POST['tid'])){$tid = htmlentities($_POST['tid'], ENT_QUOTES | ENT_IGNORE, "UTF-8");$tid = stripslashes($tid);}else{$tidget=mysql_query("SELECT MAX(tid) AS tid FROM posts LIMIT 1");$num_rows=mysql_num_rows($tidget);$tid=0;while($row=mysql_fetch_array($tidget)){$tid=$row['tid'];}++$tid;}
+	mysql_query("INSERT INTO `".$mysql['data']."`.`posts` (`title`,`name`,`email`,`date`,`content`,`ip`,`op`,`tid`) VALUES ('$title','$name','$email','$date','$content','$ip','$op','$tid')");
+	print("<h1>Posted!</h1><meta http-equiv=\"refresh\" content=\"2; URL=".$_SERVER['PHP_SELF']."?v=thread&t=".$tid."\">");
 } else {header('Location: ?v=index');}
 ?>
-<h6>Powered by ninechan v1.2 @ <a href="http://flashii.net/">Flashwave</a> 2014</h6>
+<h6>Powered by ninechan <?php if($ninechan['showversion']){print("v1.3&nbsp;");} ?>&copy; <a href="http://flashwave.pw/">Flashwave</a> 2014</h6>
 </body>
 </html>
