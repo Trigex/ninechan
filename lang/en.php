09ca3c6cb737d21b9b1d0d77b6d56d6344b7c393
Julian van de Groep <julianvdg@gmail.com>
2014-02-10 21:10:48 +0100

    Created en.php
    English language file

---
+++ lang/en.php
@@ -0,0 +1,48 @@
+<?php
+//// Ninechan Official English Language File ////
+// Board //
+define('L_THREADS', 'Threads'); // Threads
+define('L_THREAD', 'Thread'); // Thread
+define('L_NEWTHREAD', 'New Thread'); // New Thread
+define('L_NEWREPLY', 'New Reply'); // New Reply
+define('L_LOCKED', 'Locked'); // Locked
+define('L_BY', 'by'); // by
+
+// Posting //
+define('L_RETO', 'Reply to'); // Reply to
+define('L_TITLE', 'Title'); // Title
+define('L_NAME', 'Name'); // Name
+define('L_EMAIL', 'Email'); // Name
+define('L_COMMENT', 'Comment'); // Comment
+define('L_REQUIRED', 'Required'); // Required
+define('L_SUBMIT', 'Submit'); // Submit
+
+// Messages //
+define('L_BOARD_CLOSED', 'The '.$ninechan['title'].' boards are closed right now.'); // Boards are closed
+define('L_REASON', 'Reason'); // Reason
+define('L_BANNED', 'You have been banned from this board.'); // Text displayed upon being banned
+define('L_EMPTY', 'There are no threads.'); // Text displayed when the board is empty
+define('L_NONEXISTENT', 'Non-existent thread.'); // Text displayed when the board is empty
+define('L_LOCKEDMSG', 'The thread you\'re trying to reply to is locked.'); // Text displayed when the board is empty
+define('L_POSTED', 'Posted!'); // Text displayed when the post is successful
+define('L_INVALIDTITLE', 'Invalid title!'); // Text displayed when the name field is empty or invalid
+define('L_NOCOMMENT', 'No comment entered!'); // Text displayed when the comment field is empty or invalid
+define('L_MODTOOLS', 'The moderator tools should now appear next to posts.'); // Mod tools message
+
+// Warnings and errors //
+define('L_PHP_OUTDATED', 'Please upgrade your PHP installation to at least 5.2 or higher.'); // Outdated PHP version
+define('L_SQL_FUNCTION', 'Your PHP installation does not support MySQL.'); // SQL connect function does not exist
+define('L_SQL_CONNECT', 'SQL Connection Error'); // Error while connecting to MySQL
+define('L_SQL_DATABASE', 'Database Error'); // Database Error
+
+// Moderator tools //
+define('L_DELETE', 'Delete'); // Delete button
+define('L_PURGE', 'Purge Thread'); // Purge button
+define('L_LOCK', 'Lock Thread'); // Lock button
+define('L_UNLOCK', 'Unlock Thread'); // Unlock button
+define('L_BAN', 'Ban'); // Ban button
+define('L_UNBAN', 'Unban'); // Unban button
+define('L_MODLOGOUT', 'Moderator Logout'); // Moderator Logout
+define('L_MODLOGIN', 'Moderator Login'); // Moderator Login
+define('L_LOGOUT', 'Logout'); // Logout
+define('L_LOGIN', 'Login'); // Login
