b0cbf2d12fdb82dd33f3ab8300ddf4fe9e416b9a
Julian van de Groep <julianvdg@gmail.com>
2014-02-10 21:09:53 +0100

    Updated to 1.7
    Added language setting
    Added ability to disable the board description
    Show version is now disabled by default

--- config.php
+++ config.php
@@ -9,13 +9,15 @@ $mysql['table']="posts"; // MySQL Table
 
 //// Board Data ////
 $ninechan=array();
+$ninechan['lang'] = "en"; // Define the language. Must match filename from file in /lang/ folder (excluding .php).
 $ninechan['closed'] = false; // Define whether the board should be open(false) or closed(true)
 $ninechan['closedreason'] = "Maintenance"; // Specify the message displayed when the board is closed
 $ninechan['title'] = "ninechan"; // Specify the board title
+$ninechan['descenable'] = true; // Choose whether a description should be displayed or not
 $ninechan['desc'] = "ninechan best shitty board software"; // Specify the board description
 $ninechan['styleenable'] = true; // Choose whether a CSS Stylesheet should be used or not
 $ninechan['style'] = "ninechan.css"; // Specify the CSS (can be external)
-$ninechan['showversion'] = true; // Specify whether the version number is shown or not
+$ninechan['showversion'] = false; // Specify whether the version number is shown or not
 $ninechan['sage'] = false; // Specify whether the threads should "disappear" after a certain amount of new threads
 $ninechan['sagelimit'] = 20; // If ['sage'] is set to true, how many threads should be displayed
 $ninechan['titleminlength'] = 3; // Set the minimum character length of a title
