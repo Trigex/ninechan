e8d9aa0f1043b3eb4c50d3eeecaf586e50e0c0e8
Kevin López Brante <kelopez.cl@gmail.com>
2014-02-13 11:10:15 -0300

    Created es.php
    Spanish language file

---
+++ lang/es.php
@@ -0,0 +1,48 @@
+<?php
+//// Ninechan Official Spanish Language File ////
+// Board //
+define('L_THREADS', 'Hilos'); // Threads
+define('L_THREAD', 'Hilo'); // Thread
+define('L_NEWTHREAD', 'Nuevo hilo'); // New Thread
+define('L_NEWREPLY', 'Nueva respuesta'); // New Reply
+define('L_LOCKED', 'Bloqueado'); // Locked
+define('L_BY', 'por'); // by
+
+// Posting //
+define('L_RETO', 'Responder a'); // Reply to
+define('L_TITLE', 'Título'); // Title
+define('L_NAME', 'Nombre'); // Name
+define('L_EMAIL', 'Email'); // Name
+define('L_COMMENT', 'Comentario'); // Comment
+define('L_REQUIRED', 'Requerido'); // Required
+define('L_SUBMIT', 'Enviar'); // Submit
+
+// Messages //
+define('L_BOARD_CLOSED', 'Los foros '.$ninechan['title'].' se encuentran cerrados en estos momentos.'); // Boards are closed
+define('L_REASON', 'Razón'); // Reason
+define('L_BANNED', 'Se te ha suspendido el acceso a los foros.'); // Text displayed upon being banned
+define('L_EMPTY', 'No hay hilos.'); // Text displayed when the board is empty
+define('L_NONEXISTENT', 'No existe este hilo.'); // Text displayed when the board is empty
+define('L_LOCKEDMSG', 'El hilo al que estás intentando responder está bloqueado.'); // Text displayed when the board is empty
+define('L_POSTED', '¡Publicado!'); // Text displayed when the post is successful
+define('L_INVALIDTITLE', '¡Título inválido!'); // Text displayed when the name field is empty or invalid
+define('L_NOCOMMENT', '¡No escribiste nada!'); // Text displayed when the comment field is empty or invalid
+define('L_MODTOOLS', 'Las herramientas de moderación deben aparecer ahora junto a los mensajes.'); // Mod tools message
+
+// Warnings and errors //
+define('L_PHP_OUTDATED', 'Por favor, actualiza la instalación de PHP a por lo menos la versión 5.2 o superior.'); // Outdated PHP version
+define('L_SQL_FUNCTION', 'Tu instalación de PHP no soporta MySQL.'); // SQL connect function does not exist
+define('L_SQL_CONNECT', 'Error de conexión SQL'); // Error while connecting to MySQL
+define('L_SQL_DATABASE', 'Error de base de datos'); // Database Error
+
+// Moderator tools //
+define('L_DELETE', 'Eliminar'); // Delete button
+define('L_PURGE', 'Deshacerse del hilo'); // Purge button
+define('L_LOCK', 'Bloquear hilo'); // Lock button
+define('L_UNLOCK', 'Desbloquear hilo'); // Unlock button
+define('L_BAN', 'Suspender acceso'); // Ban button
+define('L_UNBAN', 'Levantar suspensión de acceso'); // Unban button
+define('L_MODLOGOUT', 'Cerrar sesión como moderador'); // Moderator Logout
+define('L_MODLOGIN', 'Iniciar sesión como moderador'); // Moderator Login
+define('L_LOGOUT', 'Cerrar sesión'); // Logout
+define('L_LOGIN', 'Iniciar sesión'); // Login
