f8c7a8b11cacfe72ccf44999806d4199a0f058a0
Kevin López <kelopez.cl@gmail.com>
2014-05-05 23:32:05 -0400

    Updated Spanish translation for 1.9

--- lang/es.php
+++ lang/es.php
@@ -1,4 +1,4 @@
-<?php
+ï»¿<?php
 //// Ninechan Official Spanish Language File ////
 // Board //
 define('L_THREADS', 'Hilos'); // Threads
@@ -10,39 +10,46 @@ define('L_BY', 'por'); // by
 
 // Posting //
 define('L_RETO', 'Responder a'); // Reply to
-define('L_TITLE', 'Título'); // Title
+define('L_TITLE', 'TÃ­tulo'); // Title
 define('L_NAME', 'Nombre'); // Name
 define('L_EMAIL', 'Email'); // Name
 define('L_COMMENT', 'Comentario'); // Comment
-define('L_REQUIRED', 'Requerido'); // Required
 define('L_SUBMIT', 'Enviar'); // Submit
+define('L_VERIFICATION', 'VerificaciÃ³n'); // Verification
+define('L_PASSWORD', 'ContraseÃ±a'); // Password
+define('L_PASSWORDCONTEXT', 'utilizada para poder eliminar posts'); // used for post deletion
+define('L_DELPOST', 'Eliminar post'); // Delete post
 
 // Messages //
-define('L_BOARD_CLOSED', 'Los foros '.$ninechan['title'].' se encuentran cerrados en estos momentos.'); // Boards are closed
-define('L_REASON', 'Razón'); // Reason
-define('L_BANNED', 'Se te ha suspendido el acceso a los foros.'); // Text displayed upon being banned
+define('L_BOARD_CLOSED', 'Los foros '.$ninechan['title'].' estÃ¡n cerrados en estos momentos.'); // Boards are closed
+define('L_REASON', 'RazÃ³n'); // Reason
+define('L_BANNED', 'Has sido baneado de este foro.'); // Text displayed upon being banned
 define('L_EMPTY', 'No hay hilos.'); // Text displayed when the board is empty
-define('L_NONEXISTENT', 'No existe este hilo.'); // Text displayed when the board is empty
-define('L_LOCKEDMSG', 'El hilo al que estás intentando responder está bloqueado.'); // Text displayed when the board is empty
-define('L_POSTED', '¡Publicado!'); // Text displayed when the post is successful
-define('L_INVALIDTITLE', '¡Título inválido!'); // Text displayed when the name field is empty or invalid
-define('L_NOCOMMENT', '¡No escribiste nada!'); // Text displayed when the comment field is empty or invalid
-define('L_MODTOOLS', 'Las herramientas de moderación deben aparecer ahora junto a los mensajes.'); // Mod tools message
+define('L_NONEXISTENT', 'No existe el hilo.'); // Text displayed when the board is empty
+define('L_LOCKEDMSG', 'El hilo al que intentaste responder estÃ¡ bloqueado.'); // Text displayed when the thread is locked and replying isn't possible
+define('L_LOCKEDMSG_2', 'No se pueden eliminar posts en hilos bloqueados.'); // Text displayed when the thread is locked and deleting isn't possible
+define('L_POSTED', 'Â¡Posteado!'); // Text displayed when the post is successful
+define('L_INVALIDTITLE', 'Â¡TÃ­tulo invÃ¡lido!'); // Text displayed when the name field is empty or invalid
+define('L_NOCOMMENT', 'Â¡Comentario invÃ¡lido!'); // Text displayed when the comment field is empty or invalid
+define('L_MODTOOLS', 'Las herramientas de moderaciÃ³n deberÃ­an empezar a aparecer al lado de los posts.'); // Mod tools message
 
 // Warnings and errors //
-define('L_PHP_OUTDATED', 'Por favor, actualiza la instalación de PHP a por lo menos la versión 5.2 o superior.'); // Outdated PHP version
-define('L_SQL_FUNCTION', 'Tu instalación de PHP no soporta MySQL.'); // SQL connect function does not exist
-define('L_SQL_CONNECT', 'Error de conexión SQL'); // Error while connecting to MySQL
-define('L_SQL_DATABASE', 'Error de base de datos'); // Database Error
+define('L_PHP_OUTDATED', 'Por favor, actualiza tu instalaciÃ³n de PHP a por lo menos 5.3 o superior.'); // Outdated PHP version
+define('L_SQL_FUNCTION', 'Tu instalaciÃ³n de PHP no soporta MySQLi.'); // SQL connect function does not exist
+define('L_SQL_CONNECT', 'Error de conexiÃ³n SQL'); // Error while connecting to MySQL
+define('L_UDB_EXISTS', 'updatedb.php existe; o no lo has borrado o la funciÃ³n unlink fallÃ³.'); // Display if updatedb.php exists
+define('L_INVALIDCAPTCHA', 'La verificaciÃ³n fallÃ³'); // Message displayed when captcha is wrong
+define('L_DEL_SUCCEED', 'Â¡Eliminado satisfactoriamente!'); // Message displayed when post is deleted
+define('L_DEL_FAILED', 'No se pudo eliminar.'); // Message displayed when post isn't deleted
 
 // Moderator tools //
 define('L_DELETE', 'Eliminar'); // Delete button
-define('L_PURGE', 'Deshacerse del hilo'); // Purge button
+define('L_PURGE', 'Purgar hilo'); // Purge button
 define('L_LOCK', 'Bloquear hilo'); // Lock button
 define('L_UNLOCK', 'Desbloquear hilo'); // Unlock button
-define('L_BAN', 'Suspender acceso'); // Ban button
-define('L_UNBAN', 'Levantar suspensión de acceso'); // Unban button
-define('L_MODLOGOUT', 'Cerrar sesión como moderador'); // Moderator Logout
-define('L_MODLOGIN', 'Iniciar sesión como moderador'); // Moderator Login
-define('L_LOGOUT', 'Cerrar sesión'); // Logout
-define('L_LOGIN', 'Iniciar sesión'); // Login
+define('L_BAN', 'Banear'); // Ban button
+define('L_UNBAN', 'Desbanear'); // Unban button
+define('L_MODLOGOUT', 'Salir'); // Moderator Logout
+define('L_MODLOGIN', 'Entrar como moderador'); // Moderator Login
+define('L_LOGOUT', 'Salir'); // Logout
+define('L_LOGIN', 'Entrar'); // Login
