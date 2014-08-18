4dc77c5191e1136d1c489820b1784cfd41bb51eb
Flashwave <julianvdg@gmail.com>
2014-08-19 00:03:15 +0200

    This one as well

--- lang/nl.php
+++ lang/nl.php
@@ -30,9 +30,11 @@ define('L_NONEXISTENT', 'Niet bestaande thread.'); // Text displayed when the bo
 define('L_LOCKEDMSG', 'De thread waarop u probeert te reageren is gesloten.'); // Text displayed when the board is empty
 define('L_LOCKEDMSG_2', 'De thread waarin uw post was is gelockt en u kunt uw post niet verwijderen.'); // Text displayed when the thread is locked and deleting isn't possible
 define('L_POSTED', 'Gepost!'); // Text displayed when the post is successful
-define('L_INVALIDTITLE', 'Foutieve title!'); // Text displayed when the name field is empty or invalid
-define('L_NOCOMMENT', 'Geen bericht ingevoerd!'); // Text displayed when the comment field is empty or invalid
 define('L_MODTOOLS', 'De moderator hulp middelen zouden nu bij de posts staan.'); // Mod tools message
+define('L_TITLETOOSHORT', 'De gegeven titel is te kort.') // The given title is too short.
+define('L_TITLETOOLONG', 'De gegeven titel is te lang.') // The given title is too long.
+define('L_COMMENTTOOSHORT', 'Het gegeven bericht is te kort.') // The given comment is too short.
+define('L_COMMENTTOOLONG', 'Het gegeven bericht is te lang.') // The given comment is too long.
 
 // Warnings and errors //
 define('L_PHP_OUTDATED', 'Upgrade uw PHP versie naar ten minste 5.3.'); // Outdated PHP version
