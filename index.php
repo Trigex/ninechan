2bbb0cbbaa68762d6bed5e66a57bcc668a915ec9
Flashwave <julianvdg@gmail.com>
2014-08-19 01:01:39 +0200

    THE DIFFERENCE ONE ! CAN MAKE also 1.10.1

--- index.php
+++ index.php
@@ -334,7 +334,7 @@ $auth = @$_SESSION['mod'];	// Set an alias for mod
 							print '<fieldset id="'.$post['id'].'">';
 							print '<legend><b>'.$post['title'].'</b> <a href="#'.$post['id'].'">'.L_BY.'</a> <b>';
 							
-							if(empty($post['email']))
+							if(!empty($post['email']))
 								print '<a href="mailto:'.$post['email'].'">'.$postData['name'].$postData['trip'].'</a>';
 							else
 								print $postData['name'].$postData['trip'];
@@ -634,6 +634,6 @@ $auth = @$_SESSION['mod'];	// Set an alias for mod
 		}
 		?>
 		<!-- Please retain the full copyright notice below including the link to flashii.net. This not only gives respect to the amount of time given freely by the developer but also helps build interest, traffic and use of ninechan. -->
-		<h6><a href="http://nine.flashii.net/" target="_blank">ninechan</a> <?=($ninechan['showversion'] ? '1.10 ' : null);?>&copy; <a href="http://flashii.net/" target="_blank">Flashwave</a></h6>
+		<h6><a href="http://nine.flashii.net/" target="_blank">ninechan</a> <?=($ninechan['showversion'] ? '1.10.1 ' : null);?>&copy; <a href="http://flashii.net/" target="_blank">Flashwave</a></h6>
 	</body>
 </html>
