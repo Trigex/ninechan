6cdc1cae54d5384bd59b72d1f89f8d35cfc48a49
Julian van de Groep <julianvdg@gmail.com>
2015-02-11 13:24:04 +0100

    1.11alpha4

--- lang/en.php
+++ lang/en.php
@@ -2,7 +2,7 @@
 //// Ninechan Official English Language File ////
 
 // Meta //
-define('LDATA_VERSION', '1.11alpha2'); // Version of ninechan language file is made for
+define('LDATA_VERSION', '1.11alpha4'); // Version of ninechan language file is made for
 
 // Board //
 define('L_THREADS', 'Threads'); // Threads
@@ -11,6 +11,7 @@ define('L_NEWTHREAD', 'New Thread'); // New Thread
 define('L_NEWREPLY', 'New Reply'); // New Reply
 define('L_LOCKED', 'Locked'); // Locked
 define('L_BY', 'by'); // by
+define('L_RETURN', 'Return to Index'); // Return to Index
 
 // Posting //
 define('L_RETO', 'Reply to'); // Reply to
@@ -25,38 +26,39 @@ define('L_PASSWORDCONTEXT', 'used for post deletion'); // used for post deletion
 define('L_DELPOST', 'Delete post'); // Delete post
 
 // Messages //
-define('L_BOARD_CLOSED', 'The '.$ninechan['title'].' boards are closed right now.'); // Boards are closed
+define('L_BOARD_CLOSED', 'The board is closed at the moment.'); // The board is closed at the moment.
 define('L_REASON', 'Reason'); // Reason
-define('L_BANNED', 'You have been banned from this board.'); // Text displayed upon being banned
-define('L_POSTBANNED', '(USER WAS BANNED FOR THIS POST)'); // Text displayed under banned post
-define('L_EMPTY', 'There are no threads.'); // Text displayed when the board is empty
-define('L_NONEXISTENT', 'Non-existent thread.'); // Text displayed when the board is empty
-define('L_LOCKEDMSG', 'The thread you\'re trying to reply to is locked.'); // Text displayed when the thread is locked and replying isn't possible
-define('L_LOCKEDMSG_2', 'The thread your post was in is locked and deletion isn\'t possible.'); // Text displayed when the thread is locked and deleting isn't possible
-define('L_POSTED', 'Posted!'); // Text displayed when the post is successful
-define('L_MODTOOLS', 'The moderator tools should now appear next to posts.'); // Mod tools message
+define('L_BANNED', 'You have been banned from this board.'); // You have been banned from this board.
+define('L_POSTBANNED', '(USER WAS BANNED FOR THIS POST)'); // (USER WAS BANNED FOR THIS POST)
+define('L_EMPTY', 'There are no threads.'); // There are no threads.
+define('L_NONEXISTENT', 'The requested thread does not exist.'); // The requested thread does not exist.
+define('L_LOCKEDMSG', "The thread you're trying to reply to is locked."); // The thread you're trying to reply to is locked.
+define('L_LOCKEDMSG_2', "The thread your post was in is locked and deletion isn't possible."); // The thread your post was in is locked and deletion isn't possible.
+define('L_POSTED', 'Posted!'); // Posted!
+define('L_MODTOOLS', 'The moderator tools should now appear next to posts.'); // The moderator tools should now appear next to posts.
 define('L_TITLETOOSHORT', 'The given title is too short.'); // The given title is too short.
 define('L_TITLETOOLONG', 'The given title is too long.'); // The given title is too long.
 define('L_COMMENTTOOSHORT', 'The given comment is too short.'); // The given comment is too short.
 define('L_COMMENTTOOLONG', 'The given comment is too long.'); // The given comment is too long.
+define('L_NOTNUMERIC', 'Could not retrieve post data because the ID you requested is not numeric, check your URL and try again.'); // Could not retrieve post data because the ID you requested is not numeric, check your URL and try again.
 
 // Warnings and errors //
-define('L_PHP_OUTDATED', 'Please upgrade your PHP installation to at least 5.3 or higher.'); // Outdated PHP version
-define('L_SQL_FUNCTION', 'Your PHP installation does not support PDO.'); // SQL connect function does not exist
-define('L_SQL_CONNECT', 'SQL Connection Error'); // Error while connecting to MySQL
-define('L_INVALIDCAPTCHA', 'Verification failed'); // Message displayed when captcha is wrong
-define('L_DEL_SUCCEED', 'Successfully deleted!'); // Message displayed when post is deleted
-define('L_DEL_FAILED', 'Failed to delete.'); // Message displayed when post isn't deleted
-define('L_USERBANNEDMSG', 'You are banned from posting on this board.'); // Message displayed on top of the board when IP is banned
-define('L_USERBANNED', 'Couldn\'t complete this action because IP is banned.'); // Message displayed when trying to access a restricted page
+define('L_PHP_OUTDATED', 'Please upgrade your PHP installation to at least 5.3 or higher.'); // Please upgrade your PHP installation to at least 5.3 or higher.
+define('L_SQL_FUNCTION', 'Your PHP installation does not support PDO.'); // Your PHP installation does not support PDO.
+define('L_SQL_CONNECT', 'SQL Connection Error'); // SQL Connection Error
+define('L_INVALIDCAPTCHA', 'Verification failed'); // Verification failed
+define('L_DEL_SUCCEED', 'Successfully deleted!'); // Successfully deleted!
+define('L_DEL_FAILED', 'Failed to delete.'); // Failed to delete.
+define('L_USERBANNEDMSG', 'You are banned from posting on this board.'); // You are banned from posting on this board.
+define('L_USERBANNED', "Couldn't complete this action because IP is banned."); // Couldn't complete this action because IP is banned.
 
 // Moderator tools //
-define('L_DELETE', 'Delete'); // Delete button
-define('L_PURGE', 'Purge Thread'); // Purge button
-define('L_LOCK', 'Lock Thread'); // Lock button
-define('L_UNLOCK', 'Unlock Thread'); // Unlock button
-define('L_BAN', 'Ban'); // Ban button
-define('L_UNBAN', 'Unban'); // Unban button
+define('L_DELETE', 'Delete'); // Delete
+define('L_PURGE', 'Purge Thread'); // Purge Thread
+define('L_LOCK', 'Lock Thread'); // Lock Thread
+define('L_UNLOCK', 'Unlock Thread'); // Unlock Thread
+define('L_BAN', 'Ban'); // Ban
+define('L_UNBAN', 'Un-ban'); // Un-ban
 define('L_MODLOGOUT', 'Moderator Logout'); // Moderator Logout
 define('L_MODLOGIN', 'Moderator Login'); // Moderator Login
 define('L_LOGOUT', 'Logout'); // Logout
